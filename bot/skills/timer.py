b334120ab152e6a3cc3aa23271e3f7f2a9ba957c
Edward Yudinkov <anstertum2@gmail.com>
2022-11-23 13:44:52 +0300

    fix: changed timer

--- bot/skills/timer.py
+++ bot/skills/timer.py
@@ -7,42 +7,50 @@ from mode import cleanup_queue_update
 from typing import Optional
 
 logger = logging.getLogger(__name__)
-
-SEC_IN_MINUTE = 60
+sec_in_minutes = 60
 
 
 def add_timer(upd: Updater, handlers_group: int):
     logger.info("registering timer handler")
-
     dp = upd.dispatcher
-
     dp.add_handler(CommandHandler("timer", timer, run_async=True), handlers_group)
 
 
-def timer(update: Update, context: CallbackContext):
-    logger.info("making timer")
+def alarm(context: CallbackContext):
+    job_context = context.job.context
 
+    job_context["cmd"].delete()
+    job_context["message"].delete()
+
+    message: Optional[Message] = context.bot.send_message(job_context["chat_id"], "Стрелочка вернулась на место")
+
+    cleanup_queue_update(
+        context.job_queue,
+        None,
+        message,
+        30,
+    )
+
+
+def timer(update: Update, context: CallbackContext):
     try:
         minutes = int(" ".join(context.args))
         message: Optional[Message] = update.message.reply_text(
-            f"Стрелочка вращается, запустил таймер на {minutes} мин", disable_notification=False)
-        remaining_time = SEC_IN_MINUTE * minutes
+            f"Стрелочка вращается, запустил таймер на {minutes} мин", disable_notification=False
+        )
+        cmd = update.message
 
         context.job_queue.run_once(
-            lambda _: context.bot.send_message(update.effective_chat.id, "Стрелочка вернулась на место"),
-            remaining_time
-        )
-
-        cleanup_queue_update(
-            context.job_queue,
-            update.message,
-            message,
-            remaining_time,
-            remove_cmd=True,
-            remove_reply=True,
+            alarm,
+            sec_in_minutes * minutes,
+            context={
+                "chat_id": update.effective_chat.id,
+                "cmd": cmd,
+                "message": message
+            }
         )
 
     except Exception as err:
-        logger.error("can't create timer: %s", err)
+        logger.info("can't create timer: %s", err)
         context.bot.send_message(update.effective_chat.id, "Не пиши глупости, укажи нормальное значение для таймера")
-        update.message.delete()
+        cmd.delete()
